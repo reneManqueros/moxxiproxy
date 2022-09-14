f69042f1b774c8348721679f88253074307cf608
rene manqueros <rene@manqueros.com>
2022-09-13 18:33:56 -0600

    Set flags via cobra

--- cmd/run.go
+++ cmd/run.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"moxxiproxy/models"
-	"strconv"
 	"strings"
 	"sync"
 )
@@ -12,45 +11,19 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run proxy server",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Defaults
-		listenAddress := "0.0.0.0:1989"
-		exitnodesFile := "./exitnodes.yml"
-		isVerbose := false
+		listenAddress, _ := cmd.Flags().GetString("address")
+		exitnodesFile, _ := cmd.Flags().GetString("exitnodes")
+		whitelist, _ := cmd.Flags().GetString("whitelist")
+		auth, _ := cmd.Flags().GetString("auth")
+		timeout, _ := cmd.Flags().GetInt("timeout")
+		isVerbose, _ := cmd.Flags().GetBool("verbose")
+		isUpstream, _ := cmd.Flags().GetBool("upstream")
+
 		username := ""
 		password := ""
-		timeout := 0
-		whitelist := ""
-		isUpstream := false
-		// Overrides
-		for _, v := range args {
-			argumentParts := strings.Split(v, "=")
-			if len(argumentParts) == 2 {
-				if argumentParts[0] == "address" {
-					listenAddress = argumentParts[1]
-				}
-
-				if argumentParts[0] == "exitnodes" {
-					exitnodesFile = argumentParts[1]
-				}
-				if argumentParts[0] == "timeout" {
-					timeout, _ = strconv.Atoi(argumentParts[1])
-				}
-				if argumentParts[0] == "auth" {
-					if authParts := strings.Split(argumentParts[1], ":"); len(authParts) > 1 {
-						username = authParts[0]
-						password = authParts[1]
-					}
-				}
-				if argumentParts[0] == "whitelist" {
-					whitelist = argumentParts[1]
-				}
-				if argumentParts[0] == "verbose" && argumentParts[1] == "true" {
-					isVerbose = true
-				}
-				if argumentParts[0] == "upstream" && argumentParts[1] == "true" {
-					isUpstream = true
-				}
-			}
+		if authParts := strings.Split(auth, ":"); len(authParts) > 1 {
+			username = authParts[0]
+			password = authParts[1]
 		}
 
 		s := models.Proxy{
@@ -75,12 +48,18 @@ var runCmd = &cobra.Command{
 				ByInstanceID: map[string]models.ExitNode{},
 			},
 		}
-
 		s.Run()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
+	runCmd.PersistentFlags().String("address", "0.0.0.0:1989", "--address=1989")
+	runCmd.PersistentFlags().String("exitnodes", "./exitNodes.yml", "--exitnodes=./exitnodes.yml")
+	runCmd.PersistentFlags().String("auth", "", "--auth=user:pass")
+	runCmd.PersistentFlags().String("whitelist", "", "--whitelist=1.2.3.4,5.6.7.8")
+	runCmd.PersistentFlags().Int("timeout", 0, "--timeout=0")
+	runCmd.PersistentFlags().Bool("verbose", false, "--verbose=false")
+	runCmd.PersistentFlags().Bool("upstream", false, "--upstream=false")
 	runCmd.Flags()
 }
